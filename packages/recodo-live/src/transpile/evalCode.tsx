42cd7d3af6b032c311c8a00c83d7d66e764058dd
ZxBing0066 <ZxBing0066@gmail.com>
2022-08-03 16:33:32 +0800

    feat(live): ✨ optimize jsx block & anonymous function compiler

--- packages/recodo-live/src/transpile/evalCode.tsx
+++ packages/recodo-live/src/transpile/evalCode.tsx
@@ -3,6 +3,59 @@ import React from 'react';
 import { Modules, Scope } from '../interface';
 import { _poly } from './transform';
 
+const cleanMultipleLineComment = (codeRef: { v: string }) => {
+    const _code = codeRef.v.replace(/^\/\*(.|\s)*?\*\//, '').trim();
+    const tag = _code.length !== codeRef.v.length;
+    codeRef.v = _code;
+    return tag;
+};
+const cleanSingleLineComment = (codeRef: { v: string }) => {
+    const _code = codeRef.v.replace(/^\/\/.*/, '').trim();
+    const tag = _code.length !== codeRef.v.length;
+    codeRef.v = _code;
+    return tag;
+};
+
+const cleanPreComment = (codeRef: { v: string }) => {
+    return cleanMultipleLineComment(codeRef) || cleanSingleLineComment(codeRef);
+};
+
+const isSingleAnonymousFunction = (code: string) => {
+    if (!code.startsWith('!function') || !code.endsWith('}')) return false;
+    const stack: string[] = [];
+    const quoteMap: Record<string, 1> = { '"': 1, "'": 1, '`': 1 };
+    let firstBeginAngleBracket = false;
+    const l = code.length;
+    for (let i = 0; i < l; i++) {
+        const s = code[i];
+        const latestSymbol = stack[stack.length - 1];
+        // is in quote
+        if (latestSymbol && quoteMap[latestSymbol]) {
+            if (s === latestSymbol) {
+                stack.pop();
+            } else {
+                stack.push(s);
+            }
+        } else {
+            if (s === '{') {
+                if (!firstBeginAngleBracket) {
+                    firstBeginAngleBracket = true;
+                } else {
+                    stack.push(s);
+                }
+            } else if (s === '}') {
+                if (!firstBeginAngleBracket) return false;
+                if (stack.length === 0 && i === l - 1) return true;
+                if (latestSymbol === '{') {
+                    stack.pop();
+                    continue;
+                }
+                return false;
+            }
+        }
+    }
+};
+
 const evalCode = (code: string, scope: Scope = {}, modules: Modules = {}) => {
     const scopeKeys = Object.keys(scope);
     const scopeValues = scopeKeys.map(key => scope[key]);
@@ -13,6 +66,8 @@ const evalCode = (code: string, scope: Scope = {}, modules: Modules = {}) => {
             : code.indexOf('module.exports') >= 0
             ? 'moduleExports'
             : 'other';
+
+    code = code.trim();
 
     const _require = (name: string) => {
         return modules[name];
@@ -33,21 +88,31 @@ const evalCode = (code: string, scope: Scope = {}, modules: Modules = {}) => {
             return <Component />;
         }
         default: {
-            let comp;
-            try {
-                // try is that a anonymous function or a jsx block
-                const _code = `return (${code.trim().replace(/;$/, '').replace(/^!/, '')})`;
-                const res = new Function('_poly', 'React', 'require', 'exports', 'module', ...scopeKeys, _code);
-                comp = res(_poly, React, _require, _exports, _module, ...scopeValues);
-            } catch (error) {
-                // format of manually return jsx block
-                const _code = `return (function() {${code}})();`;
-                const res = new Function('_poly', 'React', 'require', 'exports', 'module', ...scopeKeys, _code);
-                comp = res(_poly, React, _require, _exports, _module, ...scopeValues);
+            if (!code) return null;
+
+            // trim and clean latest ;
+            let _code = code.trim().replace(/;$/, '');
+            const codeRef = { v: _code };
+            // clean comment
+            while (cleanPreComment(codeRef)) {
+                //
             }
+            _code = codeRef.v;
+            console.log(code, _code);
+            // return jsx or a anonymous function
+            if (_code.startsWith('React.createElement(') || isSingleAnonymousFunction(_code)) {
+                _code = `return (${codeRef.v.replace(/^!function/, 'function')})`;
+            } else {
+                // format of manually return jsx block
+                _code = `return (function() {${code}})();`;
+            }
+            const res = new Function('_poly', 'React', 'require', 'exports', 'module', ...scopeKeys, _code);
+            const comp = res(_poly, React, _require, _exports, _module, ...scopeValues);
+            if (!comp) return null;
             // when return a class component or a function component
-            if (comp.prototype instanceof React.Component || typeof comp === 'function')
+            if (comp.prototype instanceof React.Component || typeof comp === 'function') {
                 return React.createElement(comp);
+            }
 
             return comp;
         }
