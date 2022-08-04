5a6f44f164ee681cddeeb0d9857f49b996823c5a
ZxBing0066 <ZxBing0066@gmail.com>
2022-08-04 17:38:12 +0800

    fix(gen): 🐞 fix options missing prop

--- packages/recodo-gen/optionsToScope.js
+++ packages/recodo-gen/optionsToScope.js
@@ -1,20 +1,13 @@
 const { isComponent: _isComponent, isDoc: _isDoc } = require('./utils');
 
-const getScopeByOptions = ({ componentPath, targetPath, componentRegExp, docRegExp, babelrc, resolver }) => {
+const getScopeByOptions = ({ componentRegExp, docRegExp, ...rest }) => {
     const isComponent = path => _isComponent(path, new RegExp(componentRegExp));
     const isDoc = path => _isDoc(path, new RegExp(docRegExp));
 
     const scope = {
-        // path of component directory
-        componentPath,
-        // path of target directory for put build file
-        targetPath,
-        // custom babel file
-        babelrc,
-        // custom resolver
-        resolver,
         isComponent,
-        isDoc
+        isDoc,
+        ...rest
     };
     return scope;
 };
